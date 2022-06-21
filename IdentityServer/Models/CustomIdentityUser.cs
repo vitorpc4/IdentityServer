7f19a6e634fa53729be9db8ff320bb114c8b2097
Vitor Henrique <vitorpc4@hotmail.com>
2022-06-20 22:47:57 -0300

    Implementando criação de tabela
    Está sendo possível criar o banco de identificação sem problemas, entretanto é necessário verificar o problema de criar o nome da tabela personalizado.

---
+++ IdentityServer/Models/CustomIdentityUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityServer.Models
+{
+    public class CustomIdentityUser : IdentityUser<int>
+    {
+        public DateTime BirthDate { get; set; }
+    }
+}
