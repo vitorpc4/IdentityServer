66adaefe32e9632810f21803a4471b14f84bbf7f
Vitor Henrique <vitorpc4@hotmail.com>
2022-06-21 23:03:52 -0300

    Implementando registro de usuário
    Agora é possível realizar o registro dos usuários, a token de ativação do e-mail também é gerada, mas não foi implementando o SMTP para o envio da token.

---
+++ IdentityServer/Profiles/RegisterProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using IdentityServer.Data.Dtos;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityServer.Profiles
+{
+    public class RegisterProfile : Profile
+    {
+        public RegisterProfile()
+        {
+            CreateMap<RegisterDto, User>();
+            CreateMap<User, IdentityUser<int>>();
+            CreateMap<User, CustomIdentityUser>();
+        }
+    }
+}
