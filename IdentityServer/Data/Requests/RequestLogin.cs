9d689228fcfa64c47d0d1b37090e695ba95ef73b
Vitor Henrique <vitorpc4@hotmail.com>
2022-06-25 23:58:57 -0300

    Implementando esqueleto para realizar o login
    O próximo passo é gerar um serviço de token, para gerar nossa própria Token com o que for necessário.

---
+++ IdentityServer/Data/Requests/RequestLogin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityServer.Data.Requests
+{
+    public class RequestLogin
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Compare("Password")]
+        [Required]
+        public string RePassword { get; set; }
+    }
+}
---
+++ IdentityServer/Services/LoginService.cs
@@ -0,0 +1,13 @@
+﻿using FluentResults;
+using IdentityServer.Data.Requests;
+
+namespace IdentityServer.Services
+{
+    public class LoginService
+    {
+        public Result LoginUser(RequestLogin requestLogin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
